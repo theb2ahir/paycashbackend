31e9924df8f46c3ff98b191f204ea6be9964d4fc
theb2ahir <baahirboukari4@gmail.com>
2026-01-22 15:20:00 +0000

    correction de bug

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ import recharges from './routes/recharges.js';
 import transfer from './routes/transfer.js';
 import retrait from './routes/retrait.js';
 import dashboard from './routes/dashboard.js';
-import sendnotif from './routes/transactionnotif.js';
-const admin = require("firebase-admin");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const port = process.env.PORT || 3000;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 
 app.get("/", (req, res) => {
   res.send("✅ PayCash SIMULATION server running");
@@ -36,7 +30,7 @@ app.use("/paycashNOTIF", sendnotif);
 
 
 app.listen(PORT, () => {
-  console.log(`🚀 Simulation server running on http://localhost:${PORT}`);
+  console.log(`🚀  server running on port ${port}`);
 });
 
 
